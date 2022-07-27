91bbd6e6a5a01bd29e52c71d1dc15c59540417e2
aboyd52501 <aboyd52501@gmail.com>
2022-07-27 15:49:16 -0500

    4.4 helper functions and unite tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,11 @@
 // I removed the unused variable because airbnb eslint didn't like it
 const dummy = () => 1;
 
+const totalLikes = (blogs) => (
+  blogs.reduce((acc, x) => acc + x.likes, 0)
+);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
