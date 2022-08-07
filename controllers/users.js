d978c86f5374e4b62c0a9deff89af059a1f59646
aboyd52501 <aboyd52501@gmail.com>
2022-08-07 13:54:37 -0500

    4.16 bloglist expansion step 4

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,17 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body;
 
-  if (!username) return res.status(400).json({ error: 'username is required' });
+  if (!username || !password) {
+    return res.status(400).json({
+      error: 'both username and password are required',
+    });
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return res.status(400).json({
+      error: 'username and password must both be over 3 characters long',
+    });
+  }
 
   const existingUser = await User.findOne({ username });
   if (existingUser) {
